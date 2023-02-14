63c620f144020dee9198fe5c78837f53eb167aad
JeevanMalla <jeevanmalla007@gmail.com>
2023-02-14 22:52:29 +0530

    Modal Window

---
+++ Day-4_Modal_Window/script.js
@@ -0,0 +1,22 @@
+'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnclose = document.querySelector('.close-modal');
+const btnopen = document.querySelectorAll('.show-modal');
+const openmodal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closemodal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+for (let i = 0; i < btnopen.length; i++)
+  btnopen[i].addEventListener('click', openmodal);
+btnclose.addEventListener('click', closemodal);
+overlay.addEventListener('click', closemodal);
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closemodal();
+  }
+});
